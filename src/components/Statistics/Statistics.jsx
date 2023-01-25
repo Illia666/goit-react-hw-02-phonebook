90c2429a0fb990ec1c1d7ee8a8a98d32387459de
Illia666 <108007460+Illia666@users.noreply.github.com>
2023-01-25 15:40:13 +0200

    dws.

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,26 +1,24 @@
 import PropTypes from 'prop-types';
 import styles from './statistics.module.css';
 
-
-const Statistics = props=>{
-    const {positivePercentage, ...opts}=props;
-    return(
-        <ul className={styles.list}>
-            {Object.defineProperties(opts).map(([key,val])=>{
-                return(
-                    <li key={key} className={styles.item}>
-                        {key}:<span className={styles.value}>{val}</span>
-                    </li>
-                );
-            })}
+const Statistics = props => {
+  const { positivePercentage, ...opts } = props;
+  return (
+    <ul className={styles.list}>
+      {Object.entries(opts).map(([key, val]) => {
+        return (
+          <li key={key} className={styles.item}>
+            {key}: <span className={styles.value}> {val}</span>
+          </li>
+        );
+      })}
       <li key="PositivePercentage" className={styles.item}>
         Positive Feedback:
         <span className={styles.value}> {positivePercentage}%</span>
       </li>
     </ul>
-    );
+  );
 };
-
 
 export default Statistics;
 
