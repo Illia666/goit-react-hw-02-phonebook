45d21b365f0e82280db23232e729fb95567d8b2c
Illia666 <108007460+Illia666@users.noreply.github.com>
2023-01-25 15:11:15 +0200

    AXSX

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,63 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+import Section from './Section/Section';
+import Options from './Options/Options';
+import Statistics from './Statistics/Statistics';
+import Notification from './Notification/Notification';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  calcTotal = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  increaseFeedback = name => {
+    this.setState(prevState => {
+      return { [name]: prevState[name] + 1 };
+    });
+  };
+
+  calcPercent = name => {
+    const total = this.calcTotal();
+    if (total === 0) return 0;
+    const value = this.state[name];
+    const part = (value / total) * 100;
+    const result = part.toFixed(0);
+    return Number(result);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.calcTotal();
+    const positiveFeedback = this.calcPercent('good');
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <Options
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.increaseFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          {total > 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positiveFeedback}
+            />
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Section>
+      </>
+    );
+  }
+}
